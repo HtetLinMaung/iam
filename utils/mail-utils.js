1cedc9832f4fc531361bf2a9bb9f908c35ea68f6
hlm <hlm@mit.com.mm>
2021-10-27 16:17:02 +0630

    update

--- utils/mail-utils.js
+++ utils/mail-utils.js
@@ -1,22 +1,26 @@
 const nodemailer = require("nodemailer");
 
 exports.sendMail = async (to, subject, html) => {
-  const transporter = nodemailer.createTransport({
-    service: process.env.EMAIL_SERVICE,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+  try {
+    const transporter = nodemailer.createTransport({
+      service: process.env.EMAIL_SERVICE,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
 
-  const mailOptions = {
-    from: process.env.EMAIL_USER,
-    to,
-    subject,
-    html,
-  };
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to,
+      subject,
+      html,
+    };
 
-  const info = await transporter.sendMail(mailOptions);
-  console.log(info.response);
-  return info;
+    const info = await transporter.sendMail(mailOptions);
+    console.log(info.response);
+    return info;
+  } catch (err) {
+    console.log(err);
+  }
 };
